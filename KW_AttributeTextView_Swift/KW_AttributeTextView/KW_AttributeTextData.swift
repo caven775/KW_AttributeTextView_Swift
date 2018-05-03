c0e184853db50f7596738723a8272c6c798cb7c0
lin_udo <linkewen@udomedia.com.cn>
2018-05-03 15:51:11 +0800

    新建demo工程

---
+++ KW_AttributeTextView_Swift/KW_AttributeTextView/KW_AttributeTextData.swift
@@ -0,0 +1,13 @@
+//
+//  KW_AttributeTextData.swift
+//  KW_AttributeTextView_Swift
+//
+//  Created by LKW on 2018/5/3.
+//  Copyright © 2018年 Udo. All rights reserved.
+//
+
+import UIKit
+
+class KW_AttributeTextData: NSObject {
+
+}
---
+++ KW_AttributeTextView_Swift/KW_AttributeTextView/KW_AttributeTextView.swift
@@ -0,0 +1,21 @@
+//
+//  KW_AttributeTextView.swift
+//  KW_AttributeTextView_Swift
+//
+//  Created by LKW on 2018/5/3.
+//  Copyright © 2018年 Udo. All rights reserved.
+//
+
+import UIKit
+
+class KW_AttributeTextView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
